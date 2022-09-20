d1cf277bdb3741efcf93b80b4919aac7b98edd72
umermalik053 <um4273209@gmail.com>
2022-09-20 12:17:58 -0700

    abc

--- server.mjs
+++ server.mjs
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   console.log("someone is requestion you:  " + req.ip);
-  console.log("someone is requestion your comsputer :  " + req.ip);
+  console.log("someone is requestion your computer :  " + req.ip);
+  console.log("someone is request your computer :  " + req.ip);
   res.send('respond from umar malik  ' + req.ip)
 })
 
