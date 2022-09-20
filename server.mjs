7ed89eaf93c5b2b8a53bc546ce90b3679755efb8
umermalik053 <um4273209@gmail.com>
2022-09-20 12:11:15 -0700

    ok

--- server.mjs
+++ server.mjs
@@ -1,12 +1,15 @@
 import express from 'express'
 const app = express()
-const port = process.env.PORT  || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  console.log("someone is requestion you:  "  + req.ip);
-  res.send('respond from umar malik  '  +  req.ip)
+  console.log("someone is requestion you:  " + req.ip);
+  console.log("someone is requestion your comsputer :  " + req.ip);
+  res.send('respond from umar malik  ' + req.ip)
 })
 
 app.listen(port, () => {
+
   console.log(`Example app listening on port ${port}`)
+
 })
